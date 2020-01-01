c7c7a3531f04861366ec5bc84d682f5ddec342d0
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-01 15:57:42 -0500

    Added feature to find games above  price

--- bin/run.rb
+++ bin/run.rb
@@ -9,11 +9,12 @@ def help(user)
     puts "help : Gets this list of options"
     puts "most_owned : Finds the most owned games"
     puts "top_games : Displays the top x games"
+    puts "expensive : Finds games above x price"
     puts "user : Shows currently logged in user"
     puts "login : Logs in or change user to compare games"
     puts "logout : Logs out of user"
     if user!= nil 
-        #puts following commands as well
+        #puts following commands if user is logged in 
         puts "additional logged in features below:"
         puts "duplicate : Finds and displays duplicate games"
         puts "collection_worth : Finds the total value of this user's collection"
@@ -48,6 +49,8 @@ def run
                 Collection.most_owned_game_printer
             when 'top_games'
                 Collection.top_x_games_chart 
+            when 'expensive'
+                Game.print_expensive_games 
             when 'duplicate' 
                 if user != nil 
                     user.print_duplicate_games    
--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,22 @@ class Game < ActiveRecord::Base
 
     has_many :collections
     has_many :users, through: :collections 
+
+
+    def self.find_expensive_games
+        input = 0 
+        loop do 
+            puts "Please enter an amount: "
+            input = get_input.to_i
+            break input if input > 0 
+        end 
+        return self.all.select{|game| game.price > input }
+    end 
+
+    def self.print_expensive_games 
+
+        expensive_games = self.find_expensive_games 
+        puts "These are the games worth more than what you asked:"
+        expensive_games.each {|game| puts game.name}
+    end 
 end 
